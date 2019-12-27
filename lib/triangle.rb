c24fc4e4769d0379b343346aa6c09663cc28444c
Daniel Reina <dannyflatiron@gmail.com>
2019-12-27 21:31:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -31,7 +31,7 @@ attr_accessor :side_1, :side_2, :side_3
         @side_1 < 0 || @side_2 < 0 || @side_3 < 0
         raise TriangleError
       elsif
-        @side_1 + @side_2 <= @side_3 || @side_3 + @side_2 <= @side_1 || @side_3 + @side_1 <= @side_1
+        @side_1 + @side_2 <= @side_3 || @side_3 + @side_2 <= @side_1 || @side_3 + @side_1 <= @side_2
         raise TriangleError
       elsif
         @side_3 == @side_2 && @side_3 == @side_1
