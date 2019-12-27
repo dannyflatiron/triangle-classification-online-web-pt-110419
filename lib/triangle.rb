7564eca84a062c95844d0f6bbe595c77df692f43
Daniel Reina <dannyflatiron@gmail.com>
2019-12-27 21:01:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,8 +15,9 @@ attr_accessor :side_1, :side_2, :side_3
       # what makes a triangle equal on all sides?
       if @side_1 == 0 && @side_2 == 0 && @side_3 == 0
         raise TriangleError
-        
-      if @side_3 == @side_2 && @side_3 == @side_1
+
+      elsif
+        @side_3 == @side_2 && @side_3 == @side_1
         :equilateral
       elsif
        @side_2 == @side_3 && @side_3 != @side_1 || @side_1 == @side_3 && @side_3 != @side_2 || @side_1 == @side_2 && @side_2 != @side_3
