820f08d2c1b8ba45ea09562b93b9cb153fc82cdf
Daniel Reina <dannyflatiron@gmail.com>
2019-12-27 20:10:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
 class Triangle
-  # write code here
+
+attr_accessor :side_1, :side_2, :side_3
+
+    def initialize(side_1, side_2, side_3)
+      @side_1 = side_1
+      @side_2 = side_2
+      @side_3 = side_3
+    end
+
+    def kind
+      # should raise a custom error
+      # how do I raise a customer error?
+      # what makes a triangle equal on all sides?
+      if @side_3 == @side_2 && @side_3 == @side_1
+        :equilateral
+      elsif
+       @side_2 == @side_3 && @side_3 != @side_1 || @side_1 == @side_3 && @side_3 != @side_2 || @side_1 == @side_2 && @side_2 != @side_3
+      #  binding.pry
+        :isosceles
+
+
+      end
+
+    end
+
+    class TriangleError < StandardError
+
+    end
 end
