075b6801a4f38cf518680bd4a90e6d2d5866aa19
Daniel Reina <dannyflatiron@gmail.com>
2019-12-27 20:36:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,12 +25,12 @@ attr_accessor :side_1, :side_2, :side_3
       elsif
         @side_1 == 0 && @side_2 == 0 && @side_3 == 0
         raise TriangleError
-      
+
       end
 
     end
 
     class TriangleError < StandardError
-
+      "triangles with no size are illegal"
     end
 end
