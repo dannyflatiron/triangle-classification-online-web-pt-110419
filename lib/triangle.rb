e73ccd718da0638401fc88f4164a3999f4bcaffc
Daniel Reina <dannyflatiron@gmail.com>
2019-12-27 20:28:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,8 +20,11 @@ attr_accessor :side_1, :side_2, :side_3
       #  binding.pry
         :isosceles
       elsif
-        @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_1 && @side_2 != @side_3 
+        @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_1 && @side_2 != @side_3
         :scalene
+      elsif
+        @side_1 == 0 && @side_2 == 0 && @side_3 == 0
+        raise TriangleError
       end
 
     end
