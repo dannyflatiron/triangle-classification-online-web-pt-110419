3b64533cfd07be466eeb5ef5b744f83366d4b2cc
Daniel Reina <dannyflatiron@gmail.com>
2019-12-27 20:58:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,7 +25,7 @@ attr_accessor :side_1, :side_2, :side_3
       elsif
         @side_1 == 0 && @side_2 == 0 && @side_3 == 0
         # how do I raise an error?
-        begin
+        
         raise TriangleError
 
       end
